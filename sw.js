7bd426e187b1d3a35940e16f9e41f2d31428455a
AnalyseDeCircuit <190540601@qq.com>
2025-10-15 18:16:57 +0800

    Site updated: 2025-10-15 18:16:57

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760519270634";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760522524448";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
